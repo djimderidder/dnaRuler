bc29605bea0230fefd706cdaa7d6281c6a014004
djimderidder <36667917+djimderidder@users.noreply.github.com>
2019-10-03 16:41:32 +0200

    quick plot

--- main_dnaRuler.m
+++ main_dnaRuler.m
@@ -12,3 +12,14 @@ data = LoadDataThunderStorm(config.nameData);
 %---find ROI---
 roi = MakeRoi(data{1}.position,config);
 dataRoi = DataInRoi(roi,data,config);
+
+%---plot ROI 1---
+figure()
+scatter(dataRoi{1}(1,:),dataRoi{1}(2,:))
+figure()
+suplot(1,2,1)
+histogram(dataRoi{1}(1,:),100)
+title('x')
+suplot(1,2,2)
+histogram(dataRoi{1}(2,:),100)
+title('y')
