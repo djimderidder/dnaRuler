2c96ed5277c30f290ac937a2036b7abe80afd825
djimderidder <36667917+djimderidder@users.noreply.github.com>
2019-10-03 15:02:33 +0200

    update1

---
+++ functions/MakeRoi.m
@@ -0,0 +1,10 @@
+function [output] = MakeRoi(positions,config)
+%for each spot create ROI
+%   Remove ROI if outside frame size
+K = config.roiSize;
+px = config.pixelSize;
+Np = config.imageSize;
+
+
+end
+
--- main_dnaRuler.m
+++ main_dnaRuler.m
@@ -1,9 +1,13 @@
 clc; clear all; close all;
+addpath(genpath(fileparts(which('main_dnaRuler'))));
 %---config---
 config.nameData = 'matlab_data_dnaRuler.mat';
+config.roiSize = 20; %square length of ROI
+config.pixelSize = 106.6666;
+config.imageSize = 255;
 
 %---loading data---
-addpath(genpath(fileparts(which('main_dnaRuler'))));
 data = LoadDataThunderStorm(config.nameData);
 
 %---find ROI---
+roi = MakeRoi(data{1}.position,conifg);
